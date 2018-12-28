da2344dd1b76b173824f920ea234ffa7bfc11072
jwin <jwin1968@gmail.com>
2018-12-28 23:00:17 +0100

    sub haskell-tree-sitter: update tree-sitter @master fix DemoEdit.hs include TSNode's id
    Markup: refactor from Char to [Text]

--- app/IntegrationTests.hs
+++ app/IntegrationTests.hs
@@ -5,7 +5,10 @@ module Main where
 
 import qualified Data.ByteString.Zipper        as Z
 import qualified Data.ByteString.UTF8          as BSU8
+import qualified Data.ByteString               as BS
 import qualified Data.ByteString.Unsafe        as BSU
+import qualified Data.Text                     as T
+import qualified Data.Text.Encoding            as TE
 
 import qualified Widgets.Edit                  as ED
 
@@ -26,11 +29,15 @@ import           Brick.Util                     ( fg
 import           Text.Pretty.Simple             ( pPrint )
 import           Control.Exception.Assert
 
+import qualified Data.Tree                     as T
+import qualified Data.Tree.Zipper              as TZ
+import           Data.List                      ( intersperse )
 
 main :: IO ()
 main =
-  let source = "odule A where --comment\nf = undefined"
-      z      = Z.byteStringZipper (BSU8.lines' $ BSU8.fromString source) Nothing
+  -- let source = "odule A where --comment \nf = undefined"
+  let source = "odule A where\nf = undefined  \n  \n "
+      z      = Z.byteStringZipper (BSU8.lines source) Nothing
       z'     = Z.insertChar 'm' z
   in  do
         let bs = BSU8.unlines $ Z.getByteString z'
@@ -41,14 +48,24 @@ main =
             tree <- hts_parse_with_language tree_sitter_haskell
                                             str
                                             (fromIntegral len)
+
             -- TODO zipper-edit, re-parse
             -- tree <- hts_parser_parse_string str (fromIntegral len)
             -- TODO tsInputEdit
+
             ts_cursor_reset_root tree cur
             (pos, (_, markup)) <- tsTransformMarkup bs cur
+
+            ts_cursor_reset_root tree cur
+            z <- tsTransformZipper cur
+            putStrLn $ T.drawTree $ TZ.toTree z
+
             return (tree, markup)
 
+        print bs
+        print (BSU8.lines bs)
         pPrint newMarkup
+        print newMarkup
         let Markup (bs, as) = newMarkup
         print $ byEq assert
                      "lengths of Markup components differ"
@@ -59,7 +76,7 @@ main =
 
 -- from brick Markup
 
-newtype Markup a = Markup ([BSU8.ByteString], [a]) deriving Show
+newtype Markup a = Markup ([T.Text], [a]) deriving Show
 
 instance Sem.Semigroup (Markup a) where
     (Markup (bs1, a1)) <> (Markup (bs2, a2)) = Markup (bs1 <> bs2, a1 <> a2)
@@ -72,7 +89,7 @@ fromByteString :: (Monoid a) => BSU8.ByteString -> Markup a
 fromByteString = (@@ mempty)
 
 (@@) :: BSU8.ByteString -> a -> Markup a
-bs @@ a = Markup ([bs], [a])
+bs @@ a = Markup ([TE.decodeUtf8 bs], [a])
 
 
 -- from Edit.hs
@@ -106,14 +123,21 @@ packNodeMarkup
   -> IO (Int, (BSU8.ByteString, Markup V.Attr))
 packNodeMarkup ptrCur nav (pos, (bs, mup)) = case nav of
 
-  TreeSitter.CursorApi.Cursor.Up   -> do
+  TreeSitter.CursorApi.Cursor.Up -> do
     p <- hasParent
     if p
       then return (pos, (bs, mup))
       else
-        let markupTail = if pos < BSU8.length bs
-              then fromByteString $ BSU8.drop pos bs
-              else mempty
+        let
+          markupTail = if pos < BSU8.length bs
+            then mconcat
+              (fmap
+                fromByteString
+                ( intersperse ("\n" :: BSU8.ByteString)
+                $ BSU8.lines' (BSU8.drop pos bs)
+                )
+              )
+            else mempty
         in  return (pos, (bs, mup <> markupTail))
 
   _ -> spanInfoAdvance bs pos ptrCur mup
@@ -130,14 +154,11 @@ spanInfoAdvance bs pos ptrCur mup = do
     Token{} -> advanceToken spanInfo
     _       -> return (pos, (bs, mup))
         -- TODO produce Error markdown-style
-        -- Error start end    -> return (pos, mup)
  where
   advanceToken (Token start end _) =
-    let (start', end') =
-          if start == 1 && end == 1 then (0, 1) else (start, end) -- TODO
-        bs'        = BSU8.drop pos bs
-        d          = start' - pos
-        dn         = end' - start'
+    let bs'        = BSU8.drop pos bs
+        d          = start - pos
+        dn         = end - start
 
         bsBefore   = BSU8.take d bs'
         markupBS   = bsBefore @@ bg V.blue
@@ -146,5 +167,5 @@ spanInfoAdvance bs pos ptrCur mup = do
         markupNode = nodeBS @@ fg V.blue
 
         mup'       = mup <> markupBS <> markupNode
-    in  return (end', (bs, mup'))
+    in  return (end, (bs, mup'))
 
